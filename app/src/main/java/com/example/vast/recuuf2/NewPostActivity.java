77d3aec79b8fc2c95a99b5b636f4b3a5d540397e
vanazco <vanazco@elpuig.xeill.net>
2019-04-09 21:56:10 +0200

    Glide de pollas

--- app/src/main/java/com/example/vast/recuuf2/NewPostActivity.java
+++ app/src/main/java/com/example/vast/recuuf2/NewPostActivity.java
@@ -1,6 +1,10 @@
 package com.example.vast.recuuf2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -14,12 +18,18 @@ import java.util.UUID;
 
 
 public class NewPostActivity  extends AppCompatActivity {
+
+    static final int RC_IMG_PICK = 9000;
+
     public Button btn_public,btn_img;
     public EditText new_title,new_context;
     public String title_string, context_string;
     public DatabaseReference mRef;
     public String id;
     public ImageView preview;
+    Uri mediaUri;
+    String mediaType;
+    Uri mFileUri;
 
 
     @Override
@@ -33,6 +43,17 @@ public class NewPostActivity  extends AppCompatActivity {
         new_title = findViewById(R.id.new_titulo);
         new_context = findViewById(R.id.new_context);
         btn_img = findViewById(R.id.btn_image);
+        preview = findViewById(R.id.img_preview);
+
+
+        btn_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI),RC_IMG_PICK);
+            }
+        });
+
+
 
         btn_public.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,5 +66,16 @@ public class NewPostActivity  extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_IMG_PICK && resultCode == RESULT_OK){
+            mediaUri = mFileUri;
+            mediaType = "image";
+
+        }
     }
 }
